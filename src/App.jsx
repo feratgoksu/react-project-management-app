74c907356b49efd8fe14824a8faf2856f9d1a30f
ferat.goksu <ferat.goksu@ibb.gov.tr>
2024-07-24 13:02:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
+function App() {
+
+  const [projectState,setProjectState]=useState({
+    selectedProjectId:undefined,
+    projects:[]
+  })
+  function handleStartAddProject() {
+    setProjectState(prevState =>{
+      return{
+        ...prevState,
+        selectedProjectId:null,
+        
+      }
+    })
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState)=>{
+      const newProject = {
+        ...projectData,
+        id:Math.random()
+      }
+      return {
+        ...prevState,
+        selectedProjectId:undefined,
+        projects:[...prevState.projects,newProject]
+      }
+    })
+  }
+
+  let content;
+  if (projectState.selectedProjectId===null) {
+    content = <NewProject onAdd={handleAddProject}/>
+    
+  } else if (projectState.selectedProjectId===undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+  return (
+    <main className="h-screen my-8 flex gap-8 ">
+    <ProjectsSidebar onStartAddProject={handleStartAddProject} projects = {projectState.projects}/>
+    {content}
+    </main>
+  );
+}
+
+export default App;
